950a49af09152f1b1978af11707570f1dd289f05
pavvloff <pavvloff@pavvloff-macbookair.roam.corp.google.com>
2015-08-11 21:39:52 +0200

    Fixed bugs in image server

--- image_server.go
+++ image_server.go
@@ -5,50 +5,88 @@ import (
     "net/http"
     "strings"
     "encoding/base64"
+    "html/template"
+    "path"
 )
 
 type PNGServer struct {
-	SavePath string
 }
 
 const (
 	PublicKey = "asfsjntvwot2352kofm245m"
+	ImageSavePath = "/home/pavvloff/image_server"
 )
 
-func (server PNGServer)ServeHTTP(writer http.ResponseWriter,request *http.Request) {
+const pageHTML = `
+<html>
+  <head>
+  <title>Created Images</title>
+  </head>
+  <body>
+  <ul>
+    {{range $file := .files}}
+      {{if not $file.IsDir}}
+        <li>
+          <img src="/image/{{$file.Name}}">
+        </li>
+      {{end}}
+    {{end}}
+  </ul>
+  </body>
+ </html>
+`
+
+func SaveImage(writer http.ResponseWriter,request *http.Request) {
 	switch request.Method {
-	case "GET":
-		writer.WriteHeader(200)
-		reader := strings.NewReader("It Works!")
-		reader.WriteTo(writer)
 	case "POST":
 		request.ParseForm()
+
 		vals := request.PostForm
 		filename := vals["filename"][0]
 		key := vals["key"][0]
 		texture := vals["texture"][0]
 		if key == PublicKey {
 			tex,_ := base64.StdEncoding.DecodeString(texture)
-			ioutil.WriteFile(server.SavePath + filename, tex, 0666)
-			
+			ioutil.WriteFile(path.Join(ImageSavePath,filename), tex, 0666)
 			writer.WriteHeader(200)
 			reader := strings.NewReader("It Works!")
 			reader.WriteTo(writer)
 		} else {
 			writer.WriteHeader(404)
-			reader := strings.NewReader("Not Found!")
+			reader := strings.NewReader("Not Found - key doesn't match!")
 			reader.WriteTo(writer)
 		}
+	default:
+		writer.WriteHeader(404)
+		reader := strings.NewReader("Not Found, but keep trying!")
+		reader.WriteTo(writer)
+	}
+}
+
+func ListImages(writer http.ResponseWriter,request *http.Request) {
+	switch request.Method {
+	case "GET":
+		html,err := template.New("results_page").Parse(pageHTML)
+		if err != nil {
+			writer.WriteHeader(500)
+			reader := strings.NewReader("Template broken!")
+			reader.WriteTo(writer)
+			return
+		}
+		writer.WriteHeader(200)
+		files,_ := ioutil.ReadDir(ImageSavePath)
+		html.Execute(writer,map[string]interface{}{"files":files})
 	default:
 		writer.WriteHeader(404)
 		reader := strings.NewReader("Not Found!")
 		reader.WriteTo(writer)
 	}
-
 }
 
 
 func main() {
-	s := PNGServer {"./"}
-	http.ListenAndServe(":8080", s)
+	http.Handle("/image/",http.StripPrefix("/image/",http.FileServer(http.Dir(ImageSavePath))))
+	http.HandleFunc("/upload",SaveImage)
+	http.HandleFunc("/list/",ListImages)
+	http.ListenAndServe(":8080",nil)
 }
